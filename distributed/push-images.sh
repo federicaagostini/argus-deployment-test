365ae1fb18ba1ca26b7fa25009fec03647a3a2d4
Marco Caberletti <marco.caberletti@gmail.com>
2016-10-21 08:46:03 +0200

    Replace docker-compose push with loop

--- distributed/push-images.sh
+++ distributed/push-images.sh
@@ -9,6 +9,11 @@ export REGISTRY="$DOCKER_REGISTRY_HOST/"
 
 echo "Push images for distributed deployment for platform $PLATFORM"
 
-docker-compose -f argus-${PLATFORM}/docker-compose.yml push
+services="pap pdp pep bdii"
+
+for srv in $services; do
+	dest=${DOCKER_REGISTRY_HOST}/italiangrid/argus-${srv}-${PLATFORM}
+	docker push $dest
+done
 
 echo "Done."
