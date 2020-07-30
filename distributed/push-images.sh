de7646220d157e03a567de3d4af24bd5027c1f1e
enricovianello <maverick17586@gmail.com>
2020-07-30 19:08:51 +0200

    Push images only to docker-hub

--- distributed/push-images.sh
+++ distributed/push-images.sh
@@ -2,18 +2,15 @@
 
 set -xe
 
-DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-"cloud-vm114.cloud.cnaf.infn.it"}
 PLATFORM=${PLATFORM:-centos7}
-
-export REGISTRY="${DOCKER_REGISTRY_HOST}/"
 
 echo "Push images for distributed deployment for platform ${PLATFORM}"
 
 services="pap pdp pep bdii"
 
 for srv in $services; do
-	dest=${DOCKER_REGISTRY_HOST}/italiangrid/argus-${srv}-${PLATFORM}:latest
-	docker push ${dest}
+  dest=italiangrid/argus-${srv}-${PLATFORM}:latest
+  docker push ${dest}
 done
 
 echo "Done."
